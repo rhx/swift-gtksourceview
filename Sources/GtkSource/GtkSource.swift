02d6f758249b5ac7a212c24072a261ed752c6599
Rene Hexel <r.hexel@griffith.edu.au>
2024-01-24 11:46:06 +1000

    Add and test get*Version() function wrappers.

--- Sources/GtkSource/GtkSource.swift
+++ Sources/GtkSource/GtkSource.swift
@@ -8,3 +8,36 @@ import CGtkSource
 /// mitigate namespace clashes with other libraries
 /// that provide a `View` type.
 public typealias SourceView = View
+
+/// Get the major version number of the GtkSourceView library.
+/// 
+/// This function wraps the underlying `gtk_source_get_major_version()`
+/// function, returning the major version of the GtkSourceView library as an `Int`.
+/// This version can be compared to the ``majorVersion`` compile-time constant.
+/// - Returns: The major version number of the GtkSourceView library.
+@inlinable
+public func getMajorVersion() -> Int {
+    return Int(gtk_source_get_major_version())
+}
+
+/// Get the minor version number of the GtkSourceView library.
+///
+/// This function wraps the underlying `gtk_source_get_minor_version()`
+/// function, returning the minor version of the GtkSourceView library as an `Int`.
+/// This version can be compared to the ``minorVersion`` compile-time constant.
+/// - Returns: The minor version number of the GtkSourceView library.
+@inlinable
+public func getMinorVersion() -> Int {
+    return Int(gtk_source_get_minor_version())
+}
+
+/// Get the micro version number of the GtkSourceView library.
+///
+/// This function wraps the underlying `gtk_source_get_minor_version()`
+/// function, returning the micro version of the GtkSourceView library as an `Int`.
+/// This version can be compared to the ``microVersion`` compile-time constant.
+/// - Returns: The micro version number of the GtkSourceView library.
+@inlinable
+public func getMicroVersion() -> Int {
+    return Int(gtk_source_get_micro_version())
+}
--- Tests/GtkSourceTests/GtkSourceTests.swift
+++ Tests/GtkSourceTests/GtkSourceTests.swift
@@ -6,7 +6,16 @@ final class GtkSourceTests: XCTestCase {
     override class func setUp() {
         usleep(100000) // FIXME: ensure gtk is initialised
     }
-    func testMajorVersion() { XCTAssertEqual(majorVersion, Int(gtk_source_get_major_version())) }
-    func testMinorVersion() { XCTAssertEqual(minorVersion, Int(gtk_source_get_minor_version())) }
-    func testMicroVersion() { XCTAssertEqual(microVersion, Int(gtk_source_get_micro_version())) }
+    func testMajorVersion() {
+        XCTAssertEqual(majorVersion, Int(gtk_source_get_major_version()))
+        XCTAssertEqual(majorVersion, getMajorVersion())
+    }
+    func testMinorVersion() {
+        XCTAssertEqual(minorVersion, Int(gtk_source_get_minor_version()))
+        XCTAssertEqual(minorVersion, getMinorVersion())
+    }
+    func testMicroVersion() {
+        XCTAssertEqual(microVersion, Int(gtk_source_get_micro_version()))
+        XCTAssertEqual(microVersion, getMicroVersion())
+    }
 }
